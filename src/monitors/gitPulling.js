844e82a9bffbf7a7d6fb2685aa12c257fc390b7b
WovenCoast <wovencoast14782@techie.com>
2019-12-13 16:16:59 +0500

    Gosh darn it

--- src/monitors/gitPulling.js
+++ src/monitors/gitPulling.js
@@ -10,7 +10,8 @@ module.exports = class extends Monitor {
 
     async run(msg) {
         if (msg.channel.id === '653558671749677056' && msg.guild.id === '630072317904683018') {
-            await exec(`git pull`, { timeout: 'timeout' in msg.flags ? Number(msg.flags.timeout) : 60000 })
+            this.client.console.log('Instantiated a restart');
+            await exec(`git pull`, { timeout: 60000 })
                 .catch(error => ({ stdout: null, stderr: error }))
             await exec(`pnpm i`, { timeout: 1000000 })
                 .catch(error => ({ stdout: null, stderr: error }));
