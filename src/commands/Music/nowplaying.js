2084b20c701786a001330172c2eb0d70cf847ba6
WovenCoast <wovencoast14782@techie.com>
2019-12-14 14:11:32 +0500

    Fixed a small bug with np

--- src/commands/Music/nowplaying.js
+++ src/commands/Music/nowplaying.js
@@ -14,7 +14,7 @@ module.exports = class extends Command {
         let serverQueue = this.client.queue.get(msg.guild.id);
         if (!serverQueue) return msg.sendLocale('NO_QUEUE');
         if (serverQueue.playing === false) return msg.channel.send(":x: Not playing anything because the queue is paused")
-        return msg.channel.send(`:arrow_forward: Now playing: **${this.parse(serverQueue.player.state.position)} / ${this.parse(serverQueue.songs[0].info.length)}** of **${serverQueue.songs[0].info.title}** requested by *${serverQueue.songs[0].info.author}*`);
+        return msg.channel.send(`:arrow_forward: Now playing: **${this.parse(serverQueue.player.state.position)} / ${this.parse(serverQueue.songs[0].info.length)}** of **${serverQueue.songs[0].info.title}** by *${serverQueue.songs[0].info.author}*`);
     }
 
     parse(length) {
