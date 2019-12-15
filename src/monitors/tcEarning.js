9baf55585b6b5c117063a006efcb2e2ac64ebb1d
WovenCoast <wovencoast14782@techie.com>
2019-12-15 13:25:33 +0500

    Implementation of TC earning

---
+++ src/monitors/tcEarning.js
@@ -0,0 +1,19 @@
+const { Monitor } = require('klasa');
+
+module.exports = class extends Monitor {
+
+    constructor(...args) {
+        super(...args, {
+            name: 'tcEarning'
+        });
+        this.percent = 30;
+    }
+
+    async run(msg) {
+        if (Math.random() < (this.percent / 100)) {
+            const amount = Math.floor(Math.random() * 9) + 1;
+            console.log(`${msg.author.tag} just got ${this.client.currency(amount)}!`);
+            msg.member.settings.update('wallet', msg.member.settings.wallet + amount);
+        }
+    }
+};
