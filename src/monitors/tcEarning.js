26525e9f66fcf70157cd0d1e243f03101430ca20
WovenCoast <wovencoast14782@techie.com>
2019-12-15 13:50:25 +0500

    Added ratelimit to it so you can't spam

--- src/monitors/tcEarning.js
+++ src/monitors/tcEarning.js
@@ -1,4 +1,5 @@
 const { Monitor } = require('klasa');
+const timeout = new Set();
 
 module.exports = class extends Monitor {
 
@@ -7,13 +8,18 @@ module.exports = class extends Monitor {
             name: 'tcEarning'
         });
         this.percent = 30;
+        this.rateLimit = 10;
     }
 
     async run(msg) {
+        if (timeout.has(`${msg.guild.id}-${msg.author.id}`)) return;
+
         if (Math.random() < (this.percent / 100)) {
             const amount = Math.floor(Math.random() * 9) + 1;
             console.log(`${msg.author.tag} just got ${this.client.economy.currency(amount)}!`);
             msg.member.settings.update('wallet', msg.member.settings.wallet + amount);
+            timeout.add(`${msg.guild.id}-${msg.author.id}`);
+            setTimeout(() => timeout.delete(`${msg.guild.id}-${msg.author.id}`), this.rateLimit * 1000);
         }
     }
 };
